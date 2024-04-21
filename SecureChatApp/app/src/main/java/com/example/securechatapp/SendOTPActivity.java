370a545e574503a1c9fa5e653b19f52d55568886
donutAnees <pvmohammedanees2003@gmail.com>
2024-04-21 15:14:24 +0530

    Added otp

--- SecureChatApp/app/src/main/java/com/example/securechatapp/SendOTPActivity.java
+++ SecureChatApp/app/src/main/java/com/example/securechatapp/SendOTPActivity.java
@@ -1,19 +1,35 @@
-package com.example.myapplication;
+package com.example.securechatapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.AndroidException;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.common.util.AndroidUtilsLight;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
 public class SendOTPActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    String mVerificationId;
+    PhoneAuthProvider.ForceResendingToken mResendToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +47,40 @@ public class SendOTPActivity extends AppCompatActivity {
                     Toast.makeText(SendOTPActivity.this , "Enter Mobile",Toast.LENGTH_SHORT).show();
                     return;
                 }
-                Intent intent = new Intent(getApplicationContext(), VerifyOTPActivity.class);
-                intent.putExtra("mobile",inputMobile.getText().toString());
-                startActivity(intent);
+
+                Log.v("PhoneNumber",inputMobile.getText().toString());
+
+                PhoneAuthOptions  options =
+                        PhoneAuthOptions.newBuilder(mAuth)
+                                .setPhoneNumber("+91 "+inputMobile.getText().toString())
+                                .setTimeout(60L, TimeUnit.SECONDS)
+                                .setActivity(SendOTPActivity.this).setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                                    @Override
+                                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                        Toast.makeText(SendOTPActivity.this,"OTP Verification done" , Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    @Override
+                                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                                        Toast.makeText(SendOTPActivity.this,"OTP Verification failed" , Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    @Override
+                                    public void onCodeSent(@NonNull String verificationId,
+                                                           @NonNull PhoneAuthProvider.ForceResendingToken token) {
+
+                                        mVerificationId = verificationId;
+                                        mResendToken = token;
+                                        Intent intent = new Intent(getApplicationContext(), com.example.securechatapp.VerifyOTPActivity.class);
+                                        intent.putExtra("mobile",inputMobile.getText().toString());
+                                        intent.putExtra("verificationID",mVerificationId);
+                                        intent.putExtra("resendToken",mResendToken);
+                                        startActivity(intent);
+                                    }
+
+                                }).build();
+
+                PhoneAuthProvider.verifyPhoneNumber(options);
             }
         }));
     }
--- SecureChatApp/app/src/main/java/com/example/securechatapp/VerifyOTPActivity.java
+++ SecureChatApp/app/src/main/java/com/example/securechatapp/VerifyOTPActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.example.securechatapp;
 
 import android.os.Bundle;
 import android.text.Editable;
